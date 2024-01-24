6a5d985409a0c4cdb645e40f2d006daea5e74e3e
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-01-24 16:30:22 +0300

    Add files via upload

---
+++ nvim/setup_root.sh
@@ -0,0 +1,25 @@
+sudo apt install exuberant-ctags
+sudo apt install neovim
+
+#for root
+
+#Install VimPlug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sudo mkdir -p /root/.config; sudo mkdir -p /root/.config/nvim; sudo mkdir -p /root/.config/nvim/plugged;
+sudo touch --no-create /root/.config/nvim/init.vim
+
+cat ./nvim.sh >> /root/.config/nvim/init.vim
+
+
+
+#Create dirs and config files
+mkdir -p $HOME/.config mkdir -p $HOME/.config/nvim; mkdir -p $HOME/.config/nvim/plugged;
+touch --no-create $HOME/.config/nvim/init.vim
+
+cat ./nvim.sh >> $HOME/.config/nvim/init.vim
+
+#Install VimPlug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
