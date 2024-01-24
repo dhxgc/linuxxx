cb5921816c4a702bd285a32279b0f6f688cc0f6c
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-01-24 18:04:35 +0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,4 @@
 !#/bin/bash
 
-sshpass -p "trfnbv12" ssh -o StrictHostKeyChecking=no netin@192.168.1.11 \
-'pwd; echo 192.168.1.11		frist_domain.ru >> real_shitt.govno;'
+sshpass -p "password" ssh -o StrictHostKeyChecking=no user@ip \
+'commands1; commands2;'
