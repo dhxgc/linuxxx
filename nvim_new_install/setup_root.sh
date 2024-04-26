0e37d6240255fa82f47662a6ce08c9dd965b3212
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-04-26 23:32:46 +0300

    Create setup_root.sh

---
+++ nvim_new_install/setup_root.sh
@@ -0,0 +1,14 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source ~/.bashrc
+nvm install node
+
+
+#Create dirs and config files
+mkdir -p $HOME/.config mkdir -p $HOME/.config/nvim; mkdir -p $HOME/.config/nvim/plugged;
+touch --no-create $HOME/.config/nvim/init.vim
+
+cat ./init.vim >> $HOME/.config/nvim/init.vim
+
+#Install VimPlug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
