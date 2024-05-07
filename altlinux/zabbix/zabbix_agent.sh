a11323d6af8706fe0aed71f7520cbf9768a49c63
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-05-08 00:44:10 +0300

    Create zabbix_agent.sh

---
+++ altlinux/zabbix/zabbix_agent.sh
@@ -0,0 +1,10 @@
+read -p "Введите IP-адрес Zabbix-сервера: " server_ip
+read -p "Введите номер клиента Zabbix: " num_client
+
+echo '
+LogFile=/var/log/zabbix/zabbix_agentd.log
+Server='$server_ip'
+ServerActive='$server_ip'
+Hostname='$num_client'_client' > /etc/zabbix/zabbix_agentd.conf
+
+systemctl --now enable zabbix_agentd
