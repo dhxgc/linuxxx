5d44a153f9ed0d2dc7d9abc35b36d8606489b2d3
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-05-08 20:04:08 +0300

    Update fwd_zabbix.sh

--- altlinux/zabbix/fwd_zabbix.sh
+++ altlinux/zabbix/fwd_zabbix.sh
@@ -2,5 +2,6 @@
 
 apt-get install firewalld -y
 systemctl --now enable firewalld
-firewall-cmd --add-service=zabbix-server --add-service=zabbix-server --permanent
+firewall-cmd --add-service=zabbix-server --add-service=zabbix-agent --permanent
+firewall-cmd --add-service=http --add-service=https --permanent
 firewall-cmd --reload
