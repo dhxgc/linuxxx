6d95146a13649459700a353fa212405175aeb066
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-05-12 20:38:44 +0500

    Update any_install.sh

--- altlinux/any_install.sh
+++ altlinux/any_install.sh
@@ -3,3 +3,8 @@ apt-get install postgresql16-server
 /etc/init.d/postgresql initdb
 
 #Настройка сети
+--
+
+#виртуалочки + менеджер
+apt-get install libvirt libvirt-kvm libvirt-qemu
+apt-get install virt-manager
