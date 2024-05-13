0cf088461c53808426e43e79dec79522c40cecba
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-05-13 20:59:57 +0500

    Update switchETCNETtoNM.sh

--- altlinux/switchETCNETtoNM.sh
+++ altlinux/switchETCNETtoNM.sh
@@ -5,5 +5,6 @@ apt-get install NetworkManager NetworkManager-tui
 cp /etc/NetworkManager/NetworkManager.conf{,2}
 sed -e 's/etcnet-alt//g' /etc/NetworkManager/NetworkManager.conf2 > /etc/NetworkManager/NetworkManager.conf
 rm /etc/NetworkManager/NetworkManager.conf2
+systemctl --now disable network.service
 
 reboot
