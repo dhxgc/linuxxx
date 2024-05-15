6ac6a24e525ff95325c8da422f96aae3762f17c7
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-05-15 23:16:01 +0500

    Update switchETCNETtoNM.sh

--- altlinux/switchETCNETtoNM.sh
+++ altlinux/switchETCNETtoNM.sh
@@ -5,6 +5,7 @@ apt-get install NetworkManager NetworkManager-tui -y
 cp /etc/NetworkManager/NetworkManager.conf{,2}
 sed -e 's/etcnet-alt//g' /etc/NetworkManager/NetworkManager.conf2 > /etc/NetworkManager/NetworkManager.conf
 rm /etc/NetworkManager/NetworkManager.conf2
+rm -rf /etc/net/ifaces/ens33
 systemctl --now disable network.service
 
 reboot
